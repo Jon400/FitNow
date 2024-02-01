01461834c202c4f5511774d9be4a3fb7387361e9
avich <aviaimzina312@gmail.com>
2024-02-01 21:53:23 +0200

    add some things

--- lib/screens/trainer_button/ActivityTime_button.dart
+++ lib/screens/trainer_button/ActivityTime_button.dart
@@ -1,7 +1,5 @@
-import 'package:fit_now/screens/profiles/trainer_profile.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
-import '../account/index.dart';
 
 class ActivityTimeButton extends StatefulWidget {
   @override
@@ -9,15 +7,11 @@ class ActivityTimeButton extends StatefulWidget {
 }
 
 class _ActivityTimeButtonState extends State<ActivityTimeButton> {
-  int _selectedPage = 0;
-  final _pageOptions = [
-    TrainerProfileScreen(),
-    AccountScreen(),
-  ];
-
   CalendarFormat _calendarFormat = CalendarFormat.week;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
+  TimeOfDay _startWorkingTime = TimeOfDay(hour: 8, minute: 0);
+  int _hoursOfWork = 8;
 
   @override
   Widget build(BuildContext context) {
@@ -29,13 +23,13 @@ class _ActivityTimeButtonState extends State<ActivityTimeButton> {
         children: [
           ElevatedButton(
             onPressed: () {
-              _showCalendar();
+              _openCalendarScreen();
             },
             child: Text("Open Calendar"),
           ),
           Expanded(
             child: Center(
-              child: _pageOptions[_selectedPage],
+              child: Text("Current Page Content"),
             ),
           ),
         ],
@@ -43,46 +37,149 @@ class _ActivityTimeButtonState extends State<ActivityTimeButton> {
     );
   }
 
-  void _showCalendar() {
-    showModalBottomSheet(
+  void _openCalendarScreen() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => CalendarScreen(
+          startWorkingTime: _startWorkingTime,
+          hoursOfWork: _hoursOfWork,
+          onHoursSelected: (startWorkingTime, hoursOfWork) {
+            setState(() {
+              _startWorkingTime = startWorkingTime;
+              _hoursOfWork = hoursOfWork;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class CalendarScreen extends StatefulWidget {
+  final TimeOfDay startWorkingTime;
+  final int hoursOfWork;
+  final Function(TimeOfDay, int) onHoursSelected;
+
+  CalendarScreen({
+    required this.startWorkingTime,
+    required this.hoursOfWork,
+    required this.onHoursSelected,
+  });
+
+  @override
+  _CalendarScreenState createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  CalendarFormat _calendarFormat = CalendarFormat.week;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calendar Screen'),
+      ),
+      body: Column(
+        children: [
+          TableCalendar(
+            firstDay: DateTime.utc(2024, 1, 1),
+            lastDay: DateTime.utc(2024, 12, 31),
+            focusedDay: DateTime.now(),
+            calendarFormat: CalendarFormat.week,
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+              });
+            },
+          ),
+          if (_selectedDay != null)
+            Expanded(
+              child: Column(
+                children: [
+                  Text(
+                    'Selected Day: ${_selectedDay!.day}/${_selectedDay!.month}/${_selectedDay!.year}',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Text('Start Working Time:'),
+                  ListTile(
+                    title: Text(
+                      _getTimeOfDayString(widget.startWorkingTime),
+                    ),
+                    onTap: () {
+                      _showTimePicker(widget.startWorkingTime, (selectedTime) {
+                        setState(() {
+                          widget.onHoursSelected(selectedTime, widget.hoursOfWork);
+                        });
+                      });
+                    },
+                  ),
+                  SizedBox(height: 10),
+                  Text('Hours of Work:'),
+                  ListTile(
+                    title: Text('${widget.hoursOfWork} hours'),
+                    onTap: () {
+                      _showHoursPicker(widget.hoursOfWork, (selectedHours) {
+                        setState(() {
+                          widget.onHoursSelected(widget.startWorkingTime, selectedHours);
+                        });
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context); // Close the current screen
+            },
+            child: Text("Save Availability"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  String _getTimeOfDayString(TimeOfDay timeOfDay) {
+    return '${timeOfDay.hour}:${timeOfDay.minute.toString().padLeft(2, '0')}';
+  }
+
+  Future<void> _showTimePicker(TimeOfDay initialTime, Function(TimeOfDay) onTimeSelected) async {
+    TimeOfDay? selectedTime = await showTimePicker(
       context: context,
-      builder: (context) {
-        return Container(
-          height: 400,
-          child: Column(
-            children: [
-              TableCalendar(
-                firstDay: DateTime.utc(2024, 1, 1),
-                lastDay: DateTime.utc(2024, 12, 31),
-                focusedDay: _focusedDay,
-                calendarFormat: _calendarFormat,
-                selectedDayPredicate: (day) {
-                  return isSameDay(_selectedDay, day);
-                },
-                onDaySelected: (selectedDay, focusedDay) {
-                  setState(() {
-                    _selectedDay = selectedDay;
-                    _focusedDay = focusedDay;
-                  });
-                },
-                onFormatChanged: (format) {
-                  setState(() {
-                    _calendarFormat = format;
-                  });
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  // Implement logic to save the selected availability
-                  // You can pass _selectedDay to another screen or function to handle availability
-                  Navigator.pop(context); // Close the bottom sheet
-                },
-                child: Text("Save Availability"),
-              ),
-            ],
+      initialTime: initialTime,
+    );
+
+    if (selectedTime != null) {
+      onTimeSelected(selectedTime);
+    }
+  }
+
+  Future<void> _showHoursPicker(int initialHours, Function(int) onHoursSelected) async {
+    int? selectedHours = await showDialog<int>(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: Text('Select Hours of Work'),
+          children: List.generate(
+            12,
+                (index) => SimpleDialogOption(
+              onPressed: () => Navigator.pop(context, (index + 1) * 2), // Increment by 2 hours each option
+              child: Text('${(index + 1) * 2} hours'),
+            ),
           ),
         );
       },
     );
+
+    if (selectedHours != null) {
+      onHoursSelected(selectedHours);
+    }
   }
 }
