41df3b85a0210a470b4e2c5f0a732799bf4ab2b1
jon400 <jonatan250199@gmail.com>
2024-02-17 22:02:39 +0200

    Divided into two parts futue and past section and sorting within

--- lib/screens/profiles/trainee_profile.dart
+++ lib/screens/profiles/trainee_profile.dart
@@ -1,93 +1,150 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 import '../../models/app_user.dart';
 import '../../models/profile.dart';
 import '../../models/training_session.dart';
 import '../../services/database.dart';
 
-
 class TraineeProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Get the current user's UID
     final user = Provider.of<AppUser?>(context);
+    final DatabaseService databaseService = DatabaseService(uid: user!.uid, roleView: 'trainee');
 
-    // Create an instance of DatabaseService with the logged-in user's UID
-    final DatabaseService databaseService = DatabaseService(
-        uid: user!.uid,
-        roleView: 'trainee'
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('My Training Sessions'),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'Future Sessions'),
+              Tab(text: 'Past Sessions'),
+            ],
+          ),
+        ),
+        body: StreamBuilder<Profile>(
+          stream: databaseService.profile,
+          builder: (context, profileSnapshot) {
+            if (profileSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (profileSnapshot.hasError) {
+              return Center(child: Text('Error: ${profileSnapshot.error}'));
+            } else if (profileSnapshot.hasData && profileSnapshot.data != null) {
+              final profile = profileSnapshot.data!;
+              return TabBarView(
+                children: [
+                  _buildSessionList(context, profile, true),
+                  _buildSessionList(context, profile, false),
+                ],
+              );
+            } else {
+              return Center(child: Text('No Profile Data Available'));
+            }
+          },
+        ),
+      ),
     );
+  }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Training Sessions'),
-      ),
-      body: StreamBuilder<Profile>(
-        stream: databaseService.profile,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (snapshot.hasData && snapshot.data != null) {
-            final profile = snapshot.data!;
-            return StreamBuilder<List<TrainingSession>>(
-              stream: profile.trainingSessions, // Assuming this is a Stream<List<TrainingSession>>
-              builder: (context, sessionSnapshot) {
-                if (sessionSnapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                } else if (sessionSnapshot.hasError) {
-                  return Center(child: Text('Error: ${sessionSnapshot.error}'));
-                } else if (sessionSnapshot.hasData) {
-                  if (sessionSnapshot.data!.isEmpty) {
-                    return Center(child: Text('No Training Sessions Found'));
-                  }
-                  return ListView.builder(
-                    itemCount: sessionSnapshot.data!.length,
-                    itemBuilder: (context, index) {
-                      TrainingSession session = sessionSnapshot.data![index];
-                      return Card(
-                        child: ListTile(
-                          leading: Icon(Icons.fitness_center),
-                          title: Text('Session ID: ${session.tid}'),
-                          subtitle: Text('Sport: ${session.sport}'),
-                          trailing: Icon(Icons.arrow_forward_ios),
-                          onTap: () {
-                            // Show an additional tile in the row with the session details
-                            showModalBottomSheet(
-                              context: context,
-                              builder: (context) {
-                                return Container(
-                                  padding: EdgeInsets.all(16),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text('Session ID: ${session.tid}'),
-                                      Text('Sport: ${session.sport}'),
-                                      Text('Specialization: ${session.spec}'),
-                                      Text('Start Time: ${session.startTime}'),
-                                      Text('End Time: ${session.endTime}'),
-                                    ],
-                                  ),
-                                );
-                              },
-                            );
-                          },
-                        ),
-                      );
-                    },
-                  );
-                } else {
-                  return Center(child: Text('No Training Sessions Available'));
-                }
-              },
-            );
-          } else {
-            return Center(child: Text('No Profile Data Available'));
+  Widget _buildSessionList(BuildContext context, Profile profile, bool isFuture) {
+    return StreamBuilder<List<TrainingSession>>(
+      stream: profile.trainingSessions,
+      builder: (context, sessionSnapshot) {
+        if (sessionSnapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (sessionSnapshot.hasError) {
+          return Center(child: Text('Error: ${sessionSnapshot.error}'));
+        } else if (sessionSnapshot.hasData) {
+          if (sessionSnapshot.data!.isEmpty) {
+            return Center(child: Text('No Training Sessions Found'));
           }
-        },
+          final now = DateTime.now();
+          final sessions = sessionSnapshot.data!.where((session) {
+            return isFuture ? session.startTime.isAfter(now) : session.startTime.isBefore(now);
+          }).toList()
+            ..sort((a, b) => isFuture ? a.startTime.compareTo(b.startTime) : b.startTime.compareTo(a.startTime));
+
+          return ListView.builder(
+            itemCount: sessions.length,
+            itemBuilder: (context, index) {
+              TrainingSession session = sessions[index];
+              return _buildSessionItem(context, session);
+            },
+          );
+        } else {
+          return Center(child: Text('No Training Sessions Available'));
+        }
+      },
+    );
+  }
+
+  Widget _buildSessionItem(BuildContext context, TrainingSession session) {
+    final startTimeFormatted = DateFormat('MMM dd, yyyy - hh:mm a').format(session.startTime);
+    final endTimeFormatted = DateFormat('MMM dd, yyyy - hh:mm a').format(session.endTime);
+
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.fitness_center),
+        title: StreamBuilder<Profile>(
+          stream: session.getTrainer(),
+          builder: (context, trainerSnapshot) {
+            if (trainerSnapshot.connectionState == ConnectionState.waiting) {
+              return Text('Loading...');
+            } else if (trainerSnapshot.hasError) {
+              return Text('Error: ${trainerSnapshot.error}');
+            } else if (trainerSnapshot.hasData) {
+              final trainer = trainerSnapshot.data!;
+              return Text('Trainer: ${trainer.firstName} ${trainer.lastName}');
+            } else {
+              return Text('No Trainer Data Available');
+            }
+          },
+        ),
+        subtitle: Text('Sport: ${session.sport} \nStart Time: $startTimeFormatted \nEnd Time: $endTimeFormatted'),
+        trailing: Icon(Icons.arrow_forward_ios),
+        onTap: () => _showSessionDetails(context, session),
       ),
+    );
+  }
+
+  void _showSessionDetails(BuildContext context, TrainingSession session) {
+    final startTimeFormatted = DateFormat('MMM dd, yyyy - hh:mm a').format(session.startTime);
+    final endTimeFormatted = DateFormat('MMM dd, yyyy - hh:mm a').format(session.endTime);
+
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              StreamBuilder<Profile>(
+                stream: session.getTrainer(),
+                builder: (context, trainerSnapshot) {
+                  if (trainerSnapshot.connectionState == ConnectionState.waiting) {
+                    return Text('Loading...');
+                  } else if (trainerSnapshot.hasError) {
+                    return Text('Error: ${trainerSnapshot.error}');
+                  } else if (trainerSnapshot.hasData) {
+                    final trainer = trainerSnapshot.data!;
+                    return Text('Trainer: ${trainer.firstName} ${trainer.lastName}');
+                  } else {
+                    return Text('No Trainer Data Available');
+                  }
+                },
+              ),
+              Text('Sport: ${session.sport}'),
+              Text('Specialization: ${session.spec}'),
+              Text('Start Time: $startTimeFormatted'),
+              Text('End Time: $endTimeFormatted'),
+            ],
+          ),
+        );
+      },
     );
   }
 }
